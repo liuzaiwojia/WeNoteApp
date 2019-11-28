185018aaf756fdc4d931a15b63cc9830ebca5a29
Liu Jia <liuzaiwojia@sina.com>
2019-11-28 20:24:51 +0800

    更改

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
+/*
+ * @Author: liujia
+ * @Date: 2019-11-28 16:49:30
+ * @Last Modified by:   liujia
+ * @Last Modified time: 2019-11-28 16:49:30
+ * @description: 项目主入口
+ */
 import React from 'react';
-import {Text, View} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import AppContainer from './Route';
 
-class HomeScreen extends React.Component {
+export default class App extends React.Component {
   render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Home!</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
-
-export default createAppContainer(TabNavigator);
